10d051a514852320b900a0b62f17bd4787244d38
Marius Muja <mariusmuja@gmail.com>
2017-12-09 10:44:17 -0800

    Update

--- outwatch-styles/src/main/scala/outwatch/styles/ComponentStyle.scala
+++ outwatch-styles/src/main/scala/outwatch/styles/ComponentStyle.scala
@@ -16,7 +16,7 @@ trait ComponentStyle {
   type Style <: StyleSheet
 
   implicit def styleToAttr(styleA: StyleA): IO[Attribute] = {
-    Attributes.className.spaceAccum := styleA.htmlClass
+    Attributes.classAccum := styleA.htmlClass
   }
 
 }
