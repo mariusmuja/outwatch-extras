464b5f34a0480f66334ab2a659b9a467b0474d1f
Marius Muja <mariusmuja@gmail.com>
2018-08-16 00:55:20 -0700

    Don't leak subscription in Store

--- outwatch-redux/src/main/scala/outwatch/extras/redux/Store.scala
+++ outwatch-redux/src/main/scala/outwatch/extras/redux/Store.scala
@@ -1,18 +1,29 @@
 package outwatch.extras.redux
 
 import cats.effect.IO
+import monix.execution.Cancelable.IsDummy
 import monix.execution.Scheduler.Implicits.global
+import monix.execution.cancelables.CompositeCancelable
 import monix.execution.misc.NonFatal
 import org.scalajs.dom
 import outwatch.dom.{Handler, Observable}
 import outwatch.extras.>-->
 
-import scala.util.Try
-
 /**
   * Created by marius on 11/06/17.
   */
 object Store {
+
+
+  private def stateReducer[Action, State <: EvolvableState[Action, State]]: (State, Action) => State =
+    (state, action) => try {
+      state.evolve(action)
+    } catch {
+      case NonFatal(e) =>
+        dom.console.error(e.getMessage)
+        state
+    }
+
 
   def create[Action, State <: EvolvableState[Action, State]](
     initActions: Seq[Action],
@@ -20,20 +31,11 @@ object Store {
   ): IO[Action >--> State] = {
 
     Handler.create[Action](initActions: _*).map { handler =>
-
       handler.transformSource { handler =>
-        val reducer: (State, Action) => State = (state, action) => Try {
-          state.evolve(action)
-        }.recover { case NonFatal(e) =>
-          dom.console.error(e.toString)
-          state
-        }.get
-
+        val reducer = stateReducer[Action, State]
         handler
           .scan(initialState)(reducer)
-          .share
-          .startWith(Seq(initialState))
-
+          .behavior(initialState).refCount
       }
     }
   }
@@ -47,22 +49,13 @@ object Store {
 
     Handler.create[Action](initActions :_*).map { handler =>
       handler.transformSource { handler =>
-
-        val reducer: (State, Action) => State = (state, action) => Try {
-          state.evolve(action)
-        }.recover { case NonFatal(e) =>
-          dom.console.error(e.toString)
-          state
-        }.get
-
+        val reducer = stateReducer[Action, State]
         Observable.merge(handler, actionSource)
           .scan(initialState)(reducer)
-          .share
-          .startWith(Seq(initialState))
+          .behavior(initialState).refCount
       }
     }
   }
-
 
   def create[Action, Effect, State <: EvolvableStateWithEffects[Action, State, Effect]](
     initActions: Seq[Action],
@@ -70,29 +63,68 @@ object Store {
     effects: IO[Effect >--> Action]
   ): IO[Action >--> State] = {
 
-    Handler.create[Action](initActions :_*).flatMap { handler =>
+    Handler.create[Action](initActions: _*).flatMap { handler =>
       effects.map { effectHandler =>
-
         handler.transformSource { handler =>
+          val sub = CompositeCancelable()
 
-          val reducer: (State, Action) => State = (state, action) => Try {
+          val reducer: (State, Action) => State = (state, action) => try {
             val se = state.evolve(action)
-            se.effects.subscribe(e => effectHandler.observer.feed(e :: Nil))
+            val cancelable = se.effects.subscribe(
+              e => effectHandler.observer.feed(e :: Nil),
+              e => dom.console.error(e.getMessage) // just log the error, don't push it into the handler's observable, because it would stop the scan "loop"
+            )
+            if (!cancelable.isInstanceOf[IsDummy]) sub += cancelable
             se.state
-          }.recover { case NonFatal(e) =>
-            dom.console.error(e.toString)
-            state
-          }.get
+          } catch {
+            case NonFatal(e) =>
+              dom.console.error(e.getMessage)
+              state
+          }
 
           Observable.merge(handler, effectHandler)
             .scan(initialState)(reducer)
-            .share
-            .startWith(Seq(initialState))
+            .behavior(initialState).refCount
+            .doOnSubscriptionCancel(() => sub.cancel())
         }
       }
     }
   }
 
+  def create[Action, Effect, State <: EvolvableStateWithEffects[Action, State, Effect]](
+    initActions: Seq[Action],
+    initialState: State,
+    actionSource: Observable[Action],
+    effects: IO[Effect >--> Action]
+  ): IO[Action >--> State] = {
+
+    Handler.create[Action](initActions: _*).flatMap { handler =>
+      effects.map { effectHandler =>
+        handler.transformSource { handler =>
+          val sub = CompositeCancelable()
+
+          val reducer: (State, Action) => State = (state, action) => try {
+            val se = state.evolve(action)
+            val cancelable = se.effects.subscribe(
+              e => effectHandler.observer.feed(e :: Nil),
+              e => dom.console.error(e.getMessage)
+            )
+            if (!cancelable.isInstanceOf[IsDummy]) sub += cancelable
+            se.state
+          } catch {
+            case NonFatal(e) =>
+              dom.console.error(e.getMessage)
+              state
+          }
+
+          Observable.merge(handler, actionSource, effectHandler)
+            .scan(initialState)(reducer)
+            .behavior(initialState).refCount
+            .doOnSubscriptionCancel(() => sub.cancel())
+        }
+      }
+    }
+  }
 
   def create[Action, Effect, State <: EvolvableStateWithEffects[Action, State, Effect]](
     initActions: Seq[Action],
@@ -106,21 +138,29 @@ object Store {
     Handler.create[Action](initActions: _*).flatMap { actions =>
       // type annotation to keep IDEA typechecker happy
       (effects.toList.sequence: IO[List[Effect >--> Action]]).map { effectHandlers =>
-
         actions.transformSource { actionSource =>
+          val sub = CompositeCancelable()
 
-          val reducer: (State, Action) => State = (state, action) => Try {
+          val reducer: (State, Action) => State = (state, action) => try {
             val se = state.evolve(action)
-            effectHandlers.foreach(effectHandler =>
-              se.effects.subscribe(e => effectHandler.observer.feed(e :: Nil))
-            )
+            effectHandlers.foreach { effectHandler =>
+              val cancelable = se.effects.subscribe(
+                e => effectHandler.observer.feed(e :: Nil),
+                e => dom.console.error(e.getMessage)
+              )
+              if (!cancelable.isInstanceOf[IsDummy]) sub += cancelable
+            }
             se.state
-          }.getOrElse(state)
+          } catch {
+            case NonFatal(e) =>
+              dom.console.error(e.getMessage)
+              state
+          }
 
           Observable.merge(actionSource :: effectHandlers: _*)
             .scan(initialState)(reducer)
-            .share
-            .startWith(Seq(initialState))
+            .behavior(initialState).refCount
+            .doOnSubscriptionCancel(() => sub.cancel())
         }
       }
     }
